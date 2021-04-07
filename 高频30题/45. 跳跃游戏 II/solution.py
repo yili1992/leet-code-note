4881ae58b24a4d0d309da3d4344895eeb7c4d50f
赵立 <zhaoli01@corp.netease.com>
2021-04-08 00:04:32 +0800

    add

---
+++ 高频30题/45. 跳跃游戏 II/solution.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: zhaoli01@corp.netease.com
+@project: leet_code
+@time: 2020-11-29 18:58
+@desc:
+"""
+
+
+class Solution:
+    def jump(self, nums: list) -> int:
+        n = len(nums)
+        maxPos, end, step = 0, 0, 0
+        for i in range(n - 1):
+            if maxPos >= i:
+                maxPos = max(maxPos, i + nums[i])
+                if i == end:
+                    end = maxPos
+                    step += 1
+        return step
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    grid = [2,3,1,2,4,2,3]
+    print(solution.jump(grid))
